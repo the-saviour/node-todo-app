b0e7a65cac3c5048cd378f9e27f245e6b9c30156
Nishant <mahat.nis@gmail.com>
2018-11-18 20:31:38 +0545

    Revert "Setup app for heroku"
    This reverts commit 874061a6dd6203363aa6febbe34a2d4034cf2c01.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@
 // connecting to TodoApp database
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds037987.mlab.com:37987/todo-app' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds037987.mlab.com:37987/todo-app' || 'mingod://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
